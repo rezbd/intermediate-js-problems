77b3c4f9503d1135120599bf06c60a7ef807f425
rezwan <rezwan6@gmail.com>
2021-06-26 18:45:39 +0600

    difference between double and triple equals

---
+++ double-triple-equals.js
@@ -0,0 +1,17 @@
+/**
+ * Double Equal:
+ * only checks value
+ * doesn't check type
+ */
+/**
+ * Triple Equal:
+ * checks value and type
+ */
+//Case 1: true is compared with 1, false is compared with 0
+const first = 0;
+const second = false;
+if (first == second) {
+	console.log("condition is true");
+} else {
+	console.log("condition is false");
+}
